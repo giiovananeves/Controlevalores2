eb13572b707ca4618677cb55024c742200819ac5
giiovananeves <giovana.silva23@fatec.sp.gov.br>
2022-03-07 19:28:12 +0000

    correto

---
+++ Tableless.java
@@ -0,0 +1,10 @@
+public class Tableless {
+    public static void main(String[] args) {
+        int x = 15;
+        while (x < 18) {
+            System.out.println("Voce não tem permissão para entrar");
+            x++;
+            
+        }
+    }
+}
